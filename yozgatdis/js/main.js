ab710e8dd4586945e331cc6fc1b3c5cb895dee84
adogan-home-windows <eagletr@msn.com>
2018-02-05 00:45:22 +0300

    Statik tasarım tamamlandı.

--- yozgatdis/js/main.js
+++ yozgatdis/js/main.js
@@ -33,158 +33,73 @@ TABLE OF CONTENT
 
 $(document).ready(function() {
 
-/*------------------------------------
-    1. Owl Carousel
---------------------------------------*/  
+    /*------------------------------------
+        1. Owl Carousel
+    --------------------------------------*/
 
 
-/*---------------------
- Methods Carousel
------------------------*/
+    /*---------------------
+     Methods Carousel
+    -----------------------*/
 
-  $('#our-methods').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3000,  
-    responsiveClass: true,
-    autoplayHoverPause:false,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 30
-      },
-      600: {
-        items: 2,
-        margin: 15
-      },
-      1000: {
-        items: 3,
-        margin: 15
-      }
-    }
-  })
-
-
-
-/*---------------------
-Gallery carousel
------------------------*/
-
-  $('#gallery').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: true,
-    autoplay: true,
-    autoplayTimeout: 3000,  
-    responsiveClass: true,
-    autoplayHoverPause:false,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 15,
-      },
-      600: {
-        items: 2,
-        margin: 30,
-      },
-      1000: {
-        items: 3,
-        margin: 30,
-      }
-    }
-  })
+    $('#our-methods').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsiveClass: true,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 30
+            },
+            600: {
+                items: 2,
+                margin: 15
+            },
+            1000: {
+                items: 3,
+                margin: 15
+            }
+        }
+    })
 
 
 
+    /*---------------------
+    Gallery carousel
+    -----------------------*/
 
-/*---------------------
-Testmonials carousel
------------------------*/
-
-  $('#testmonials-carousel').owlCarousel({
-    loop: false,
-    nav: false,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 10,
-      },
-      600: {
-        items: 2,
-        margin: 15,
-      },
-      1000: {
-        items: 3,
-        margin: 30,
-      }
-    }
-  })
-
-
-
-/*---------------------
-Testmonial-single carousel
------------------------*/
-
-  $('#testmonial-single-carousel').owlCarousel({
-    loop: false,
-    nav: false,
-    dots: false,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 0,
-      },
-      600: {
-        items: 1,
-        margin: 0,
-      },
-      1000: {
-        items: 1,
-        margin: 0,
-      }
-    }
-  })
+    $('#gallery').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsiveClass: true,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 15,
+            },
+            600: {
+                items: 2,
+                margin: 30,
+            },
+            1000: {
+                items: 3,
+                margin: 30,
+            }
+        }
+    })
 
 
-
-
-/*---------------------
-Certificates carousel
------------------------*/
-
-  $('#our-certificates').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3000,  
-    responsiveClass: true,
-    autoplayHoverPause:false,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 15,
-      },
-      600: {
-        items: 3,
-        margin: 30,
-      },
-      1000: {
-        items: 4,
-        margin: 30,
-      }
-    }
-  })
-
-
-/*------------------------------------
-    2. CountUp
---------------------------------------*/  
+    /*------------------------------------
+        2. CountUp
+    --------------------------------------*/
 
     $('.countup').counterUp({
         delay: 5,
@@ -192,151 +107,72 @@ Certificates carousel
     });
 
 
-/*------------------------------------
-    3. Pie Chart
---------------------------------------*/  
+    /*------------------------------------
+        4. Hover Drop Down
+    --------------------------------------*/
 
-    $(function(){
-      $('.circlestat').circliful();
-    });
-
-    $(".refreshButton").click(function(event){
-      location.reload(); 
-    });
-
-
-/*------------------------------------
-    4. Hover Drop Down
---------------------------------------*/    
-
-if ($(window).width() > 991) {
-  $('ul.nav li.dropdown').hover(function() {
-      $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
-  }, function() {
-      $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
-  });
-}
-
-
-/*------------------------------------
-    5. Tabs
---------------------------------------*/    
-
-  $('.tabs_animate').tabslet({
-    mouseevent: 'click',
-    attribute: 'href',
-    animation: true
-  });
-
-
-/*------------------------------------
-    6. Video Modal
---------------------------------------*/ 
-
-  $('#videomodal').on('hidden.bs.modal', function() {
-    var $this = $(this).find('iframe'),
-      tempSrc = $this.attr('src');
-    $this.attr('src', "");
-    $this.attr('src', tempSrc);
-  });
-
-  $('#videomodal').on('hidden.bs.modal', function() {
-    var html5Video = document.getElementById("htmlVideo");
-    if (html5Video != null) {
-      html5Video.pause();
-      html5Video.currentTime = 0;
+    if ($(window).width() > 991) {
+        $('ul.nav li.dropdown').hover(function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+        }, function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+        });
     }
-  });
 
 
-/*------------------------------------
-    7. Preloader
---------------------------------------*/ 
+    /*------------------------------------
+        7. Preloader
+    --------------------------------------*/
 
-  $('#preloader').fadeOut('normall', function() {
-      $(this).remove();
-  });
+    $('#preloader').fadeOut('normall', function() {
+        $(this).remove();
+    });
 
 
-/*------------------------------------
-    8. Scroll To Top
---------------------------------------*/ 
+    /*------------------------------------
+        8. Scroll To Top
+    --------------------------------------*/
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 500) {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
             $(".scroll-to-top").fadeIn(400);
-            
+
         } else {
             $(".scroll-to-top").fadeOut(400);
         }
     });
- 
-    $(".scroll-to-top").on('click', function(event){
+
+    $(".scroll-to-top").on('click', function(event) {
         event.preventDefault();
-        $("html, body").animate({scrollTop: 0},600);
+        $("html, body").animate({ scrollTop: 0 }, 600);
     });
 
 
 
+    /*------------------------------------
+        11. AhmetDGN - Contact Form
+    --------------------------------------*/
 
-
-
-
-/*------------------------------------
-    9. Isotop
---------------------------------------*/  
-
-// external js: isotope.pkgd.js
-// init Isotope
-var $grid = $('.isotope-grid').isotope({
-  itemSelector: '.isotope-item',
-  layoutMode: 'fitRows',
-  getSortData: {
-    name: '.name',
-    symbol: '.symbol',
-    number: '.number parseInt',
-    category: '[data-category]',
-    weight: function( itemElem ) {
-      var weight = $( itemElem ).find('.weight').text();
-      return parseFloat( weight.replace( /[\(\)]/g, '') );
-    }
-  }
-});
-
-// filter functions
-var filterFns = {
-  // show if name ends with -ium
-  ium: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /ium$/ );
-  }
-};
-
-// bind filter button click
-$('#filters').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
-});
-
-
-// change is-checked class on buttons
-$('.latest-projects').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
-
-
-
-
-/*------------------------------------
-    10. WOW
---------------------------------------*/ 
-new WOW().init();
-
+    $("#contact-form-main").submit(function(event) {
+        var $contactForm = $("#contact-form-main");
+        event.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/eagletr@msn.com",
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json",
+            beforeSend: function() {
+                $contactForm.append('<div class="alert alert-info">Mesaj gönderiliyor…</div>');
+            },
+            success: function(data) {
+                $contactForm.find('.alert-info').hide();
+                $contactForm.append('<div class="alert alert-success">Mesaj gönderildi!</div>');
+            },
+            error: function(err) {
+                $contactForm.find('.alert-info').hide();
+                $contactForm.append('<div class="alert alert-danger">Mesaj gönderilirken bir sorun oluştu! Lütfen daha sonra tekrar deneyin ya da telefon numaramızı veya e-posta adresimizi kullanın.</div>');
+            }
+        });
+    });
 
 });
